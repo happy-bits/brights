7027a725e646f1498fe15b9ca3c31c0716b160cb
Oscar Olsson <happybits@gmail.com>
2023-07-06 15:38:04 +0200

    -

---
+++ Checkpoint/hints-for-checkpoint.md
@@ -0,0 +1,22 @@
+# Hints for checkpoint
+
+## General
+
+On the checkpoint you'll get a HTML-file, a CSS file and an (almost) empty JavaScript file. You'll do all your work in the JavaScript-file. 
+
+You only need to solve Level 1 to pass the checkpoint.
+
+## Level 1
+
+Solve the JavaScript part for these exercises again:
+- Lab01 
+- Lab02
+- Lab03
+- Lab04
+- Email
+
+## Level 2
+
+First study for Level 1
+
+Then look at the solution for **Region**. Try to solve the exercise yourself (the Javascript-part)
---
+++ JavaScript/Lab/05/this.js
@@ -0,0 +1,23 @@
+// This code works fine, but it's a bit repetitive...
+
+const button1 = document.querySelector(".button1")
+const button2 = document.querySelector(".button2")
+const button3 = document.querySelector(".button3")
+
+function changeBackground1() {
+    button1.style.backgroundColor="#a7d31e"
+}
+
+function changeBackground2() {
+    button2.style.backgroundColor="#a7d31e"
+}
+
+function changeBackground3() {
+    button3.style.backgroundColor="#a7d31e"
+}
+
+// This code solves the same problem but with just three lines of code!
+
+function changeBackground(element) {
+    element.style.backgroundColor="#a7d31e"
+}
