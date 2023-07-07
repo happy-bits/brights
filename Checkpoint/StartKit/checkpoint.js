d8698a99c16100665134ed6440de32e171f8bcbc
Oscar Olsson <happybits@gmail.com>
2023-07-07 09:07:22 +0200

    -

---
+++ Checkpoint/StartKit/checkpoint.js
@@ -0,0 +1,11 @@
+
+const allGnomes = [
+    { file: "garden.jpg", description: "garden gnome", price: 400 },
+    { file: "lady.jpg", description: "lady gnome", price: 120 },
+    { file: "laugh.jpg", description: "laughing gnome", price: 300 },
+    { file: "military.jpg", description: "military gnome", price: 240 },
+    { file: "red-hat.jpg", description: "red hat", price: 10 },
+    { file: "summer.jpg", description: "double summer", price: 80 }
+]
+
+/* write your code here... */
