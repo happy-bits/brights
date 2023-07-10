38e3c384929e3a6372442396a492b0c34e404b7a
Oscar Olsson <happybits@gmail.com>
2023-07-10 10:42:32 +0200

    -

--- React/lab/src/email/Email.js
+++ React/lab/src/email/Email.js
@@ -1,6 +1,18 @@
 function Email() {
 
-    return <h2>Sign up</h2>
+    return (
+    
+        <article>
+
+            <h2>Sign up</h2>
+
+            <div className="mb-3">
+                <label htmlFor="emailInput" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+            </div>
+
+        </article>
+    )
 
 }
 
--- React/lab/src/index.js
+++ React/lab/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 import Email from './email/Email'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './custom.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hallå eller?</h1>
+
     <Email />
-    <Email />
-    <Email />
+
   </React.StrictMode>
 );
 
