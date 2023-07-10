44fdf1b5e246e2e7de9a85931bd940c67b854f03
Oscar Olsson <happybits@gmail.com>
2023-07-10 13:50:40 +0200

    -

--- React/lab/src/email/Email05.js
+++ React/lab/src/email/Email05.js
@@ -1,23 +1,20 @@
+/*
+    If the user clicks ok => validate the form (write in the console)
+*/
+
 import { useState } from 'react'
 
 function Email() {
 
-    const [input, setInput] = useState('')
-    const [inputAgain, setInputAgain] = useState('')
+    const [input, setInput] = useState("")
+    const [inputAgain, setInputAgain] = useState("")
 
     const inputChange = (event) => {
-        // input = event.target.value
         setInput(event.target.value)
     }
 
     const inputAgainChange = (event) => {
-        // input = event.target.value
         setInputAgain(event.target.value)
-    }
-
-    const changeEmailAddress = (event) => {
-        // input = "klas@katt.nu"
-        setInput('klas@katt.nu')
     }
 
     const sendForm = () => {
@@ -36,31 +33,31 @@ function Email() {
             console.log("Not the same")
             return
         }
-        
-        console.log("Success")
+
+        console.log("Success!")
     }
 
     return (
 
         <article className="box">
 
-            <h2>Sign up</h2>
-
+            <h1>Sign up</h1>
             <div className="mb-3">
                 <label htmlFor="emailInput" className="form-label">Email address</label>
                 <input type="email" value={input} onChange={inputChange} className="form-control" id="emailInput" placeholder="name@example.com" />
             </div>
 
+
             <div className="mb-3">
-                <label htmlFor="emailInputAgain" className="form-label">Enter again</label>
-                <input type="email" value={inputAgain} onChange={inputAgainChange} className="form-control" id="emailInputAgain" placeholder="name@example.com" />
+                <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
+                <input type="email" value={inputAgain} onChange={inputAgainChange} className="form-control" id="emailAgainInput" placeholder="name@example.com" />
             </div>
 
-            <button onClick={sendForm} type="button" className="btn btn-primary btn-lg me-1">Ok</button>
-
+            <button  onClick={sendForm} className="btn btn-primary btn-lg">Ok</button>
 
 
         </article>
+
     )
 }
 
@@ -71,7 +68,6 @@ function isValidEmailAddress(email) {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         ) != null;
 }
-
 
 
 export default Email
