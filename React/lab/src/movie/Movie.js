4ff60c55550f6f4127675beae58b9382901ae505
Oscar Olsson <happybits@gmail.com>
2023-07-11 09:15:31 +0200

    -

--- React/lab/src/movie/Movie.js
+++ React/lab/src/movie/Movie.js
@@ -1,7 +1,29 @@
+
+import poster from './posters/good-bad-ugly.jpg'
+
 function Movie() {
     return(
 
         <article className="box">
+
+            <img className='img-fluid' src={poster}/>
+
+            {/* 
+            
+            todo: skapa en div-tag med klassen "text-center"
+
+            I den div'taggen lägg in fem img-taggar
+
+            De behöver ha en css-klass (kolla i custom.css)
+            
+
+
+            Skriv "You rated 3 stars!" i en div-tag
+            Div-taggen ska ha en klass (kolla i custom.css)
+
+
+            */}
+
         </article>
     )
 }
