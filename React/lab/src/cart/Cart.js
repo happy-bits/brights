e510eb544d6968ee9f13002e5ef726320402479e
Oscar Olsson <happybits@gmail.com>
2023-07-12 11:17:14 +0200

    -

--- React/lab/src/cart/Cart.js
+++ React/lab/src/cart/Cart.js
@@ -27,9 +27,9 @@ function CartItem(props) {
                 {product.price} kr/st
             </div>
             <div className="col-3 d-flex justify-content-between">
-                <button  className="btn btn-primary btn-sm">-</button>
+                <button onClick={() => props.adjustProduct(props.id, -1) } disabled={props.amount === 0} className="btn btn-primary btn-sm">-</button>
                 <div className="px-2 align-self-center">{props.amount}</div>
-                <button className="btn btn-primary btn-sm">+</button>
+                <button onClick={() => props.adjustProduct(props.id, +1) } disabled={props.amount === maxNrOfProducts} className="btn btn-primary btn-sm">+</button>
             </div>
             <div className="col-3 text-end">
                 {props.amount * product.price} kr
@@ -42,24 +42,33 @@ function CartItem(props) {
 function Cart() {
 
     const [cart, setCart] = useState([
-        {id:"p1", amount: 30},
+        {id:"p1", amount: 3},
         {id:"p2", amount: 5},
-        {id:"p3", amount: 3},
+        {id:"p3", amount: 2},
 
     ])
+
+    const adjustProduct = (id, change) => {
+        console.log(`Adjust product with id ${id} with: ${change}`)
+    }
+
+    const totalAmount = () => {
+        // todo: försök lösa denna
+        return 1234567
+    }
 
     return (
         <article className="box" style={{ userSelect: "none" }}>
             <h1 className="display-5 mb-4">Cart</h1>
 
-            {cart.map(item => <CartItem key={item.id} id={item.id} amount={item.amount}/>)}
+            {cart.map(item => <CartItem adjustProduct={adjustProduct} key={item.id} id={item.id} amount={item.amount}/>)}
 
             <div className="row mt-4">
                 <div className="col-9">
                     Total
                 </div>
                 <div className="col-3 text-end">
-                    ... kr
+                    {totalAmount()} kr
                 </div>
             </div>
         </article>
