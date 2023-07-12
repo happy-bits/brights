dc1685eb04fc6bae78a8a9b206d682d82072947f
Oscar Olsson <happybits@gmail.com>
2023-07-12 09:18:47 +0200

    -

--- React/lab/src/cart/Cart.js
+++ React/lab/src/cart/Cart.js
@@ -1,26 +1,48 @@
+// Styling
 
 function CartItem() {
-    return(
-        <div>
-            I'm a cart item
+
+    return (
+        <div className="row my-2 align-items-center">
+            <div className="col-3">
+                ...
+            </div>
+            <div className="col-3 text-end">
+                ... kr/st
+            </div>
+            <div className="col-3 d-flex justify-content-between">
+                <button className="btn btn-primary btn-sm">-</button>
+                <div className="px-2 align-self-center">...</div>
+                <button className="btn btn-primary btn-sm">+</button>
+            </div>
+            <div className="col-3 text-end">
+                ... kr
+            </div>
+
         </div>
     )
 }
 
-
 function Cart() {
 
-    return(
-       <article className="box">
-            <h1>Cart</h1>
+    return (
+        <article className="box" style={{ userSelect: "none" }}>
+            <h1 className="display-5 mb-4">Cart</h1>
+            <CartItem />
 
-            <CartItem/>
-            
-            <div>
-                Total
+            <div className="row mt-4">
+                <div className="col-9">
+                    Total
+                </div>
+                <div className="col-3 text-end">
+                    ... kr
+                </div>
             </div>
-       </article>
+        </article>
     )
+
 }
+
+
 
 export default Cart
